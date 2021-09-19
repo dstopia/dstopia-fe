02628a24183faf4f5d727ba30cc53922185b0d11
dfsdanimaulana <danimaulana9f@gmail.com>
2021-09-19 19:54:27 +0700

    add folder pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Home from './containers/Home/Home.js'
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Home />
     </React.StrictMode>,
     document.getElementById('root')
 )
