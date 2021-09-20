5e9b36eed4677c855287f25a946a0c8689151a4a
dfsdanimaulana <danimaulana9f@gmail.com>
2021-09-20 09:30:53 +0700

    mengimplementasikan react redux

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-export default class Home extends Component {
-    render () {
-        return ( <div> Home Page </div>)
+import Navbar from '../../components/Navbar/Navbar'
+import Category from '../Pages/Category/Category'
+
+class Home extends Component {
+    constructor(props) {
+        super(props)
+        console.log(props)
+    }
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <button onClick={this.props.greet}>button</button>
+                <div>{this.props.user}</div>
+                <Router>
+                    <Route path='/category' component={Category} />
+                </Router>
+            </div>
+        )
     }
 }
+
+const mapStateToProos = (state) => {
+    // state global
+    return {
+        user: state.user,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        greet: () => dispatch({type:'GREET_USER'})
+    }
+}
+export default connect(mapStateToProos, mapDispatchToProps)(Home)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+/** Components */
 import Home from './containers/Home/Home.js'
+
+/** default Global state */
+const globalState = {
+    user: 'Dani',
+}
+
+/** Reducer */
+const rootReducer = (state = globalState, action) => {
+    switch (action.type) {
+        case 'GREET_USER':
+            return {
+                ...globalState,
+                user: `Hello ${state.user}`
+            }
+        default:
+            return state
+    }
+}
+
+/** Global Store */
+const store = createStore(rootReducer)
 
 ReactDOM.render(
     <React.StrictMode>
-        <Home />
+        <Provider store={store}>
+            <Home />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 )
