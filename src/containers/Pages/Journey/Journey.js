d250e4cdd4df2b327593f19cfebec8d46ba3f362
Irullatif <irulsoekamti94@gmail.com>
2021-10-08 17:11:11 +0700

    update Jouney page on Pages

--- src/containers/Pages/Journey/Journey.js
+++ src/containers/Pages/Journey/Journey.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import CarouselCard from '../../../components/carouselCard/Carousel'
+import CategoryCard from '../../../components/categoryCard/CategoryCard'
 
 export default function Journey() {
-    return <div>Journey Page</div>
+    return (
+        <div>
+            <CarouselCard />
+            <CategoryCard />
+        </div>
+    )
 }
